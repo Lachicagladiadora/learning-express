f73ee80b4adc77e4ca18971fabd8b961bfb538de
Lachicagladiadora <dev.roseny@gmail.com>
2025-04-18 21:00:56 -0500

    feat: add post to auth

--- examples/auth.js
+++ examples/auth.js
@@ -7,6 +7,16 @@ const port = 3000
 
 app.get('/', (req, res) => { res.send('Hello world AUTH') })
 
+// config
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+
+//
+
 app.get('/', (req, res) => {
   res.redirect('/login');
 });
@@ -26,3 +36,26 @@ app.get('/login', (req, res) => {
 });
 
 app.listen(port, () => { console.log(`I'm listening on port ${port} 🪼`) })
+
+
+
+app.post('/login', function (req, res, next) {
+  if (!req.body) return res.sendStatus(400)
+  authenticate(req.body.username, req.body.password, function (err, user) {
+    if (err) return next(err)
+    if (user) {
+      req.session.regenerate(function () {
+        req.session.user = user;
+        req.session.success = 'Authenticated as ' + user.name
+          + ' click to <a href="/logout">logout</a>. '
+          + ' You may now access <a href="/restricted">/restricted</a>.';
+        res.redirect(req.get('Referrer') || '/');
+      });
+    } else {
+      req.session.error = 'Authentication failed, please check your '
+        + ' username and password.'
+        + ' (use "tj" and "foobar")';
+      res.redirect('/login');
+    }
+  });
+});
